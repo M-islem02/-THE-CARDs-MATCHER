e615be0d58061d9ad10279b4ec8e78643047a66a
Name <mi.bounouala@esi-sba.Dz>
2026-04-24 22:14:41 +0100

    Improve responsive game layout

--- components/CardGrid.js
+++ components/CardGrid.js
@@ -1,7 +1,4 @@
 function renderCardGrid(cards, container) {
   container.innerHTML = '';
-  container.style.display = 'grid';
-  container.style.gridTemplateColumns = 'repeat(8, 1fr)';
-  container.style.gap = '8px';
   cards.forEach(card => container.appendChild(card.element));
 }
